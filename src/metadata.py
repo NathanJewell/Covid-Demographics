76423a1f7ed30f6321a06697dbba012512fc273c
NathanJewell <jewelln@oregonstate.edu>
2020-05-31 02:14:59 -0700

    finished v1 data processing

--- src/metadata.py
+++ src/metadata.py
@@ -3,6 +3,7 @@ gis_demo_data = {
     "fn" : "gis-demo-counties.csv",
     "state_col" : "state_name",
     "county_col" : "name",
+    "state_fmt" : "name",
     "load_cols" : [
         "population", "pop_sqmi" , "sqmi",
         "white", "black", "ameri_es", "asian", "hawn_pi", "hispanic", "other", "multi_race",
@@ -15,6 +16,7 @@ kaggle_demo_data = {
     "fn" : "kaggle-demo-counties.csv",
     "state_col" : "state",
     "county_col" : "county",
+    "state_fmt" : "name",
     "load_cols" : [
         "income", "incomepercap", "poverty"
     ]
@@ -24,7 +26,45 @@ covid_data = {
     "fn" : "covid-counties.csv",
     "state_col" : "county",
     "county_col" : "state",
+    "state_fmt" : "name",
     "load_cols" : [
         "date", "fips", "cases", "deaths"
     ]
 }
+
+unemployment_data = {
+    "fn" : "Unemployment.csv",
+    "state_col" : "stabr",
+    "county_col" : "area_name",
+    "filter_col" : "rural_urban_continuum_code_2013", #needed to remove state meta-entries
+    "state_fmt" : "abv",
+    "load_cols" : [
+        "median_household_income_2018",
+        "unemployment_rate_2018"
+    ]
+}
+#"Unemployment and median household income for the U.S., States, and counties, 2000-19",,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,
+#,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,
+#"Sources: Unemployment: U.S. Department of Labor, Bureau of Labor Statistics, Local Area Unemployment Statistics (LAUS); median household income: U.S. Department of Commerce, Bureau of the Census, Small Area Income and Poverty Estimates (SAIPE) Program.",,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,
+#"For definitions of rural classifications, see the USDA, Economic Research Service webpage ""Rural Classifications"" in the ""Rural Economy & Population"" topic. Variable descriptions (column names) are found in the second tab in this workbook.",,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,
+#"This table was prepared by USDA, Economic Research Service. Data as of May 13, 2020. Contact: Kathleen Kassel.",,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,
+
+education_data = {
+    "fn" : "Education.csv",
+    "state_col" : "state",
+    "county_col" : "county",
+    "state_fmt" : "abv",
+    "filter_col" : "2003 rural-urban continuum code", #needed to remove state meta-entries
+    "load_cols" : [
+        "percent_bs_higher_2014-18",
+        "percent_some_college_2014-18",
+        "percent_adults_hs_only_2014-18",
+        "percent_adults_lessthan_hs_2014-18"
+    ]
+}
+
+
+#"Educational attainment for adults age 25 and older for the U.S., States, and counties, 1970-2018","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","",""
+#"Sources: U.S. Census Bureau, 1970, 1980, 1990, 2000 Censuses of Population, and the 2014-18 American Community Survey 5-yr average county-level estimates.","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","",""
+#"For definitions of rural classifications, see the USDA, Economic Research Service webpage ""Rural Classifications"" in the ""Rural Economy & Population"" topic.","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","",""
+#"This table was prepared by USDA, Economic Research Service. Data as of February 5, 2020. Contact: Kathleen Kassel.","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","",""
--- src/parse.py
+++ src/parse.py
@@ -3,8 +3,10 @@
 
 import pandas as pd
 import numpy as np
-import fuzzywuzzy as fwp
+import fuzzywuzzy.process as fwp
 from metadata import *
+from convert_state import asState
+import sys
 
 data_path = "../data/"
 data_fp = lambda fn : "{}{}".format(data_path, fn)
@@ -21,24 +23,38 @@ def load_data(metadata):
 def clean_data(data, metadata):
     #removes columns not contained in "load_cols"
     #applies tolower for headers
+    print("CLEANING")
     for d, m in zip(data, metadata): 
-        use_cols = m["load_cols"] + [m["state_col"], m["county_col"]]
-        state_rows = []
-        for row, county in enumerate(d[m["county_col"]]):
-            if not county:
-                state_rows.append(row)
-        d.drop(rows=state_rows, inplace=True) #drop all rows without county
+        remove_col_labels = [m["state_col"], m["county_col"]]
+        if "filter_col" in m:
+            remove_col_labels.append(m["filter_col"])
+        use_cols = m["load_cols"] + remove_col_labels
+        #if m["fn"] == "Unemployment.csv":
+            #import pdb
+            #pdb.set_trace()
         remove_cols = [col_name
                 for col_name in list(d.columns) 
                 if col_name.lower() not in use_cols]
         d.drop(columns=remove_cols, inplace=True)
         #first word + lowercase to county
         d.columns = [c.lower() for c in d.columns]
-        d[m["county_col"]].apply(lambda x: x.split(' ')[0].lower())
+
+        remove_rows = []
+
+        for label in remove_col_labels:
+            for row, val in enumerate(d[label]):
+                if pd.isnull(val):
+                    remove_rows.append(row)
+        d.drop(index=remove_rows, inplace=True) #drop all rows without value in metadata->"filter_col"
+        print("\tDropped {} incomplete rows from {}".format(len(remove_rows),m["fn"]))
+
+        d[m["county_col"]].apply(lambda x: x.lower())#x.split(' ')[0].lower())
+        if (m["state_fmt"] == "abv"):
+            d[m["state_col"]] = d[m["state_col"]].apply(lambda state_abv: asState(state_abv))
 
     return data
 
-
+#UNUSED before I realized pd.merge exists
 def aggregate_counties(data, metadata):
     cols = []
     num_unique = 0
@@ -90,41 +106,100 @@ def aggregate_counties(data, metadata):
     return np.asarray(data_py, dtype=float), col_labels
 
 def aggregate_counties_pandas(data, metadata):
+    #setup some variables
     county_label = "county"
     state_label = "state"
+    unique_label = "unique"
+    unique = lambda d : "{}.{}".format(d[county_label], d[state_label])
 
     first = True
     final = None
     for d, m in zip(data, metadata):
+    #iterate over all the datasets we need to combine
         state_col = m["state_col"]
         county_col = m["county_col"]
+
+        #swap standardized key column names into df
+        # ? should this be moved to clean ?
         new_columns = list(d.columns)
         new_columns[new_columns.index(state_col)] = state_label
         new_columns[new_columns.index(county_col)] = county_label 
         d.columns = new_columns
+        d.insert(0, unique_label, d[[state_label, county_label]].apply(unique, axis=1))
+        
+        #there was an error when I tried to compare pd.Series obj to None
+        #thus the extra variable
         if first:
             final = d
             first = False
-        else:
-            final = final.merge(d, 'inner', [county_label, state_label])
+            continue
+
+        #https://stackoverflow.com/questions/35380933/how-to-merge-two-pandas-dataframes-based-on-a-similarity-function
+        state_choices = {}
+        complete = 0
+        dropped = 0
+        def ldist(row):
+            nonlocal complete
+            nonlocal dropped
+            progress_interval = int(len(d) / 100)
+            minscore = 85
+            complete += 1
+            choices = None
+            if row[state_label] in state_choices:
+                choices = state_choices[row[state_label]]
+            else:
+                choices = set(final[final[state_label] == row[state_label]][unique_label])
+                state_choices[row[state_label]] = choices
+            
+            choices = [""] if not choices else choices
+            choice, score = fwp.extractOne(row[unique_label],choices)
+
+            if ((len(d) - complete) % progress_interval) == 0:
+                #print("{}%".format(int((complete/len(d)) * 100)))
+                sys.stdout.write("\r\t\t{}%".format(int((complete/len(d)) * 100)))
+                sys.stdout.flush()
+            #print("Choice: {} Score: {}".format(choice, score))
+            if score < minscore or len(choices) < 1:
+                dropped += 1
+                return None
+                #print(score)
+
+            state_choices[row[state_label]].remove(choice)
+            return choice
+
+        print("MERGING")
+        print("\tcomputing levenshtein distances")
+        d[unique_label] = d.apply(ldist, axis=1)
+        print("\n\tdid not match {} entries".format(dropped))
+        print("\tfinished computing distances")
+        d = d[pd.notnull(d[unique_label])]
+        d.drop(columns=[state_label, county_label], inplace=True)
+        
+        final = final.merge(d, on=unique_label, how='inner')
+        print("{} counties are still matched".format(len(final)))
+
     
     return final
-
-
     
 def entry():
     #setup metadata
     metadata = [
         gis_demo_data,
-        covid_data
+        covid_data,
+        unemployment_data,
+        education_data
     ]
 
     #load data
-    data = load_data(metadata)
+    raw_data = load_data(metadata)
 
-    data = clean_data(data, metadata)
+    cleaned_data = clean_data(raw_data, metadata)
 
-    data = aggregate_counties_pandas(data, metadata)
+    pd_data = aggregate_counties_pandas(cleaned_data, metadata)
+
+    print("Saving processed data to csv")
+    pd_data.sort_values(by=['state','county'], inplace=True)
+    pd_data.to_csv(data_fp("output.csv"))
     #data_np, labels = aggregate_counties(data, metadata)
 
 
